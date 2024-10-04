cee75b088dcc568eb2cb279236c1a32c2fa9ab80
dantvi <daniel.tving@gmail.com>
2024-10-04 15:49:03 +0200

    post request for friends

--- server.js
+++ server.js
@@ -19,12 +19,30 @@ const friends = [
   }
 ];
 
-// Our Own Logging Middleware
+// Our own logging middleware
 app.use((req, res, next) => {
   const start = Date.now();
   next();
   const deltaTime = Date.now() - start;
   console.log(`method: ${req.method}, url: ${req.url}, request time: ${deltaTime}ms`);
+});
+
+// JSON parsing middleware from express
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: 'Missing friend name'
+    });
+  }
+
+  const newFriend = {
+    id: friends.length,
+    name: req.body.name       // Uses json parsing middleware
+  }
+  friends.push(newFriend);
+  res.json(newFriend);
 });
 
 app.get('/friends', (req, res) => {
