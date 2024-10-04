ce1468f6f29a30a0ed9569c6dcaf943d5fe8a8a2
dantvi <daniel.tving@gmail.com>
2024-10-04 13:06:34 +0200

    route parameters to get friends

--- server.js
+++ server.js
@@ -4,9 +4,45 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Heeelllloooooooo');
+const friends = [
+  {
+    id: '0',
+    name: 'Albert Einstein'
+  },
+  {
+    id: '1',
+    name: 'Marie Curie'
+  },
+  {
+    id: '2',
+    name: 'Sir Isaac Newton'
+  }
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
 });
+
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: 'Friend does not exist'
+    });
+  }
+});
+
+app.get('/messages', (req, res) => {
+  res.send('<ul><li>Hello! You are a cat!</li></ul>');
+});
+
+app.post('/messages', (req, res) => {
+  console.log('Updating messages...');
+  res.send('Message updated');
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
